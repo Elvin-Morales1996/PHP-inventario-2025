6ec7fa632b81592e38ca5f25cd390f1f3d7dd0c8
elvin morales <morales.elvin.16@gmail.com>
2025-03-11 18:08:05 -0600

    hola

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include "./inc/head.php";?>
+</head>
+<body >
+<?php
+    include "./inc/navbar.php";
+
+    include "./inc/script.php";
+
+?>
+    
+</body>
+</html>
